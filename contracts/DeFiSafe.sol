2b158d6f567de534a931b7bd65d8870e234e8021
shuishangmusheng <455194872@qq.com>
2020-05-05 22:08:37 +0800

    Improve insurance business

--- contracts/DeFiSafe.sol
+++ contracts/DeFiSafe.sol
@@ -69,13 +69,8 @@ contract DeFiSafe {
         require(amount > 0,"Too few assets");
         //匹配路由
         TokenPool storage pool = matchRoute(tokenType);
-        pool.tokenAmount += amount;
-        pool.userAmount += 1;
-        User storage user = pool.users[name];
-        user.name = name;
-        user.tokenID = tokenType;
-        user.amount = amount;
-        user.price = 10;
+        //资产入账
+        assetEntryBook(pool,name,tokenType,amount);
     }
 
     /*
@@ -105,27 +100,80 @@ contract DeFiSafe {
     token资金池
     作用：记账本的作用，记录当前对应token的总量，以及用户信息，以及对应用户的资产价格快照
     参数：
+    1. 对应的token记账本
     1. 地址
     2. 类型
     3. 数量
     */
+    function assetEntryBook(TokenPool pool,address name,uint tokenType,uint amount) internal {
+        //记账
+        pool.tokenAmount += amount;
+        pool.userAmount += 1;
+        User storage user = pool.users[name];
+        user.name = name;
+        user.tokenID = tokenType;
+        user.amount = amount;
+        user.price = getTokenPrice(tokenType);
+        //开始分配资产
+        assetsEnterInsurancePool(swapDai(tokenType,amount*0.05));
+        assetsEnterUniSwap(tokenType,amount*0.95);
+    }
 
     /*
-    资产分配函数
-    作用：进入池子的资产，对资产进行处理，与第三方合约进行交互。
+    uniswap
+    作用：用户资产95%流入uniswap
     参数：
     1. 类型
     2. 数量
-    3. 输入\输出
     */
+    function assetsEnterUniSwap(uint tokenType,uint amount) internal {
+        //uniswap 添加流动性
+    }
 
 
-    /*
+     /*
     投保池
     作用：盈余资金的记录与存储
     参数：
     1. 数量
     */
+    function assetsEnterInsurancePool(uint amount) internal{
+        insurePool.depositAmount += amount;
+        assetsEnterAAVE(amount);
+    }
+
+    /*
+    AAVE添加流动性
+    作用：AAVE添加流动性
+    参数：
+    1. 数量(Dai)
+    */
+    function assetsEnterAAVE(uint amount) {
+        //调取外部接口
+    }
+
+    /*
+    获取快照数据（数据来源ChainLink）
+    作用：获取当前对应Token的市场价格
+    参数：
+    1. 类型
+    */
+    function getTokenPrice(uint tokenType) public returns(uint){
+        //调取外部接口
+        return 10;//测试数据
+    }
+
+    /*
+    代币交换 token-Dai
+    作用：转换成稳定币
+    参数：
+    1. 类型
+    2. 数量
+    */
+    function swapDai(uint tokenType,uint amount) public returns(uint){
+        //调取外部接口
+        return 10;//测试数据
+    }
 
 
     /*
@@ -135,6 +183,7 @@ contract DeFiSafe {
     1. 地址
     2. 类型
     */
+    
 
     /*
     获取当前盈余资金函数
