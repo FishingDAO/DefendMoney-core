ff26a795a1f78356c733f5b1276088004dca6cd9
shuishangmusheng <455194872@qq.com>
2020-05-15 23:40:49 +0800

    1. Transfer user assets to the defer.money platform when insuring 2. Modification of uniswaputil exchange function

--- contracts/UniswapUtils.sol
+++ contracts/UniswapUtils.sol
@@ -99,7 +99,7 @@ contract UniswapUtils {
         public
         returns (uint256)
     {
-        return tokenToDai(_tokenAddress, _tokenAmount, uint256(1));
+        return _tokenToDai(_tokenAddress, _tokenAmount, uint256(1));
     }
 
     /**
@@ -109,7 +109,7 @@ contract UniswapUtils {
      * @param _minTokenOut minimum ETH bought
      * @param return amount of DAI bought
      */ 
-    function tokenToDai(
+    function _tokenToDai(
         address _tokenAddress,
         uint256 _tokenAmount,
         uint256 _minTokenOut
