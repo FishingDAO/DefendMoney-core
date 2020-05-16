777aa88b6d500fca3c81e09fe29ab3dd0bd2b4c3
shuishangmusheng <455194872@qq.com>
2020-05-16 22:15:50 +0800

    Unified version number

--- contracts/UniswapInterface.sol
+++ contracts/UniswapInterface.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.6.0;
 /**
  * @title The interface of UniswapFactory
  */
--- contracts/UniswapUtils.sol
+++ contracts/UniswapUtils.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.6.0;
 
 import "./UniswapInterface.sol";
 import "./IERC20.sol";
