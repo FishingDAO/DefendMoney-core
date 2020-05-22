ca818b8a65ac9d23fa41057766e9dd12e167e145
taosue <taosue314@gmail.com>
2020-05-22 19:33:45 +0800

    fix bug in swap token

--- contracts/UniswapUtils.sol
+++ contracts/UniswapUtils.sol
@@ -13,7 +13,21 @@ contract UniswapUtils {
     address public constant UNISWAP_FACTORY_ADDRESS = 0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351;
     // DAI ropsten testnet address
     address public constant DAI_ADDRESS = 0xaD6D458402F60fD3Bd25163575031ACDce07538D;
+    // LINK ropsten testnet address
+    address public constant LINK_ADDRESS=0x20fE562d797A42Dcb3399062AE9546cd06f63280;
+    // the address of the owner
+    address payable public owner;
     
+    IERC20 public token=IERC20(0x20fE562d797A42Dcb3399062AE9546cd06f63280);
+    
+
+    constructor() public payable {
+        owner = msg.sender;
+    }
+    
+    fallback () external payable {}
+    
+    receive () external payable {}
 
     /**
      * @dev Get UniswapExchange
@@ -28,44 +42,48 @@ contract UniswapUtils {
         return
             IUniswapFactory(UNISWAP_FACTORY_ADDRESS).getExchange(_tokenAddress);
     }
-
+    
     /**
-     * @dev ETH=>DAI
-     * @param _recipient Address that receives ERC20 tokens
-     * @return Amount of DAI bought
-     */
-    function ethToDai(_recipient) 
-        public 
-        payable
-        returns (uint256) 
-    {
-        return ethToDai(1,_recipient);
-    }
-
-    /**
-     * @dev ETH=>DAI
-     * @param _minTokenAmount minimum DAI bought
-     * @param _recipient Address that receives ERC20 tokens
-     * @return Amount of DAI bought
-     */
-    function ethToDai( uint256 _minTokenAmount,address _recipient )
+     * @dev Swap ETH from other token
+     * 
+     * @return Amount of ETH
+     */    
+    function ethToDai()
         public
         payable
         returns (uint256)
     {
-        require(msg.value > 0.001 ether);
         IUniswapExchange uniswapExchange=IUniswapExchange(getUniswapExchange(DAI_ADDRESS));
-        return uniswapExchange.ethToTokenTransferInput.value(msg.value)(_minTokenAmount, uint256(now + 600),_recipient);
+        return uniswapExchange.ethToTokenTransferInput.value(msg.value)(1, uint256(now + 600),address(this));
     }
-    
-    
-    function getPirce(uint256 eth_sold)
+    /**
+     * @dev Swap Dai from other token
+     * 
+     * @param tokens_sold Amount of ERC20 tokens sold
+     * @param token_addr Address of output ERC20 token
+     * @return Amount of token
+     */
+   function tokenToDai(uint256 tokens_sold,address token_addr)
+       public
+       returns(uint256)
+    {
+        address daiUniswapAddress=getUniswapExchange(DAI_ADDRESS);
+        IERC20(DAI_ADDRESS).approve(daiUniswapAddress, tokens_sold);
+        return IUniswapExchange(daiUniswapAddress).tokenToTokenTransferInput(tokens_sold, 1,1, uint256(now + 600),address(this),token_addr);
+    }
+    /**
+     * @dev get the price of token(DAI)
+     * @param tokens_sold Amount of ERC20 tokens sold
+     * @return Amount of DAI that can be bought
+     */
+    function getPirce(uint256 tokens_sold)
         public
         view
         returns(uint256)
     {
         IUniswapExchange uniswapExchange=IUniswapExchange(getUniswapExchange(DAI_ADDRESS));
-        return uniswapExchange.getEthToTokenInputPrice(eth_sold);
+        uint256 eth_amount=uniswapExchange.getTokenToEthInputPrice(tokens_sold);
+        return uniswapExchange.getEthToTokenInputPrice(eth_amount);
     }
     
 }
